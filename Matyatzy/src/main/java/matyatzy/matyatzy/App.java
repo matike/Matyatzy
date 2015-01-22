7c0c8380ba20fbf970a538b879a5ae4c30ff37ce
unknown <matike2@gmail.com>
2015-01-22 14:37:41 +0200

    pelin nimi muutettu

---
+++ Matyatzy/src/main/java/matyatzy/matyatzy/App.java
@@ -0,0 +1,13 @@
+package matyatzy.matyatzy;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
